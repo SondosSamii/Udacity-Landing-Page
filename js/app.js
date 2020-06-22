acb86c7bf65636cc9c4321a2ebb4fde503e2759a
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-22 21:18:37 +0200

    Edit functions

--- js/app.js
+++ js/app.js
@@ -21,12 +21,12 @@
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
+
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
-
 
 
 /**
@@ -37,31 +37,46 @@ const sections = document.querySelectorAll('section');
 
 // build the nav
 function buildNav() {
-    for (let i = 0; i < sections.length; i++) {
+    sections.forEach(function (section) {
         const newLi = document.createElement('li');
         const newA = document.createElement('a');
-        const navItem = sections[i].querySelector('h2').textContent;
+        const navItem = section.querySelector('h2').textContent;
 
         newA.classList.add('menu__link');
         newA.textContent = navItem;
         newLi.appendChild(newA);
         navbarList.appendChild(newLi);
-
-        console.log(sections[i]);
-    }
+    })
 }
 
 // Add class 'active' to section when near top of viewport
+function addActiveClass() {
+    const navLinks = document.querySelectorAll('#navbar__list a');
+    navLinks.forEach(function (navLink, i) {
+        const sectionId = sections[i].getAttribute('id');
+        navLink.setAttribute('href', `#${sectionId}`);
+
+        navLink.addEventListener('click', addClass, true);
+        navLink.removeEventListener('click', addClass, false);
+
+        function removeClass() {
+            const activeClass = document.getElementsByClassName('your-active-class');
+            activeClass[0].classList.remove('your-active-class');
+        }
+
+        function addClass() {
+            removeClass()
+            sections[i].scrollIntoView();
+            sections[i].classList.add('your-active-class');
+        }
+    })
+}
 
 
 // Scroll to anchor ID using scrollTO event
 function scrollTo() {
-    for (let i = 0; i < sections.length; i++) {
-        const sectionId = sections[i].getAttribute('id');
-        const navLinks = document.querySelectorAll('#navbar__list a');
+    //console.log(window.pageYOffset);
 
-        navLinks[i].setAttribute('href', `#${sectionId}`);
-    }
 }
 
 /**
@@ -69,6 +84,7 @@ function scrollTo() {
  * Begin Events
  *
 */
+
 
 // Build menu
 const t0 = performance.now();
@@ -79,13 +95,14 @@ console.log(`Time to execute for loop for make a navbar is ${(t1 - t0).toFixed(4
 
 // Scroll to section on link click
 const t2 = performance.now();
-setTimeout(scrollTo, 100);
-clearTimeout(scrollTo, 100);
+window.addEventListener('scroll', scrollTo, true);
+// window.removeEventListener('scroll', scrollTo, false);
 const t3 = performance.now();
 console.log(`Time to execute scrolling to section on link click ${(t3 - t2).toFixed(4)}`);
 
 // Set sections as active
-// const t4 = performance.now();
-
-// const t5 = performance.now();
-// console.log(`Time to execute adding active class is ${(t5 - t4).toFixed(4)}`);
+const t4 = performance.now();
+setTimeout(addActiveClass, 50);
+clearTimeout(addActiveClass, 50);
+const t5 = performance.now();
+console.log(`Time to execute adding active class is ${(t5 - t4).toFixed(4)}`);
